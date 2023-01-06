8771da786d2a0a90a7cd5d087aaba0c02ffdd511
邱亮 <qiuliang@qiuliangdeMacBook-Pro.local>
2023-01-06 14:26:11 +0800

    update page1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React GIT up1111
+          Learn React GIT up1111ss
         </a>
       </header>
     </div>
