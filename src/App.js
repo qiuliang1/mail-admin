3e35ab063d3adca37f4484c7f20296d925e39f94
qiuliang1 <985444288@qq.com>
2023-01-10 16:29:29 +0800

    remove logo00

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React GIT
+          Learn React GIT111
 
           <pre>
           #！/bash/sh
