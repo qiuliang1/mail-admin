ae3b766540f18624a6c589ab5bef98be2710570d
qiuliang1 <985444288@qq.com>
2023-01-10 16:04:44 +0800

    push test1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React GIT up1111ss
+          Learn React GIT
 
           <pre>
           #！/bash/sh
